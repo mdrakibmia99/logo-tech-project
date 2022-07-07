e2451545933d5cb308440f0fc022f1a8bdeae30f
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-07 21:41:00 +0600

    protfolio routes add in app.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Banner from './components/Banner';
 import Experience from './components/Experience';
+import Portfolio from './components/Portfolio';
 import Services from './components/Services';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Banner />
       <Services />
       <Experience/>
+      <Portfolio/>
     </section>
   );
 }
